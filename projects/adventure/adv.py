845923aa6ee8f1cfd360531e81c0cc50a45584cc
jeff15113 <itsmejeffery@gmail.com>
2019-05-17 15:00:20 -0400

    MVP

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,15 +1,16 @@
-from room import Room
-from player import Player
-from world import World
-
 import random
+
+from player import Player
+from room import Room
+from util import Queue
+from world import World
 
 # Load world
 world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 
-# roomGraph={0: [(3, 5), {'n': 1}], 1: [(3, 6), {'s': 0, 'n': 2}], 2: [(3, 7), {'s': 1}]}
+#roomGraph = {0: [(3, 5), {'n': 1}], 1: [(3, 6), {'s': 0, 'n': 2}], 2: [(3, 7), {'s': 1}]}
 # roomGraph={0: [(3, 5), {'n': 1, 's': 5, 'e': 3, 'w': 7}], 1: [(3, 6), {'s': 0, 'n': 2}], 2: [(3, 7), {'s': 1}], 3: [(4, 5), {'w': 0, 'e': 4}], 4: [(5, 5), {'w': 3}], 5: [(3, 4), {'n': 0, 's': 6}], 6: [(3, 3), {'n': 5}], 7: [(2, 5), {'w': 8, 'e': 0}], 8: [(1, 5), {'e': 7}]}
 # roomGraph={0: [(3, 5), {'n': 1, 's': 5, 'e': 3, 'w': 7}], 1: [(3, 6), {'s': 0, 'n': 2}], 2: [(3, 7), {'s': 1}], 3: [(4, 5), {'w': 0, 'e': 4}], 4: [(5, 5), {'w': 3}], 5: [(3, 4), {'n': 0, 's': 6}], 6: [(3, 3), {'n': 5, 'w': 11}], 7: [(2, 5), {'w': 8, 'e': 0}], 8: [(1, 5), {'e': 7}], 9: [(1, 4), {'n': 8, 's': 10}], 10: [(1, 3), {'n': 9, 'e': 11}], 11: [(2, 3), {'w': 10, 'e': 6}]}
 # roomGraph={0: [(3, 5), {'n': 1, 's': 5, 'e': 3, 'w': 7}], 1: [(3, 6), {'s': 0, 'n': 2, 'e': 12, 'w': 15}], 2: [(3, 7), {'s': 1}], 3: [(4, 5), {'w': 0, 'e': 4}], 4: [(5, 5), {'w': 3}], 5: [(3, 4), {'n': 0, 's': 6}], 6: [(3, 3), {'n': 5, 'w': 11}], 7: [(2, 5), {'w': 8, 'e': 0}], 8: [(1, 5), {'e': 7}], 9: [(1, 4), {'n': 8, 's': 10}], 10: [(1, 3), {'n': 9, 'e': 11}], 11: [(2, 3), {'w': 10, 'e': 6}], 12: [(4, 6), {'w': 1, 'e': 13}], 13: [(5, 6), {'w': 12, 'n': 14}], 14: [(5, 7), {'s': 13}], 15: [(2, 6), {'e': 1, 'w': 16}], 16: [(1, 6), {'n': 17, 'e': 15}], 17: [(1, 7), {'s': 16}]}
@@ -24,7 +25,74 @@ player = Player("Name", world.startingRoom)
 
 traversalPath = []
 
+graph = {}
+unknown_room = {}
+graph[0] = player.currentRoom.getExits()
+for exit in graph[0]:
+    unknown_room[exit] = '?'
+    graph[0] = unknown_room
 
+oppositeLookup = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
+
+
+def bfs(start):
+    visited = set()
+    queue = Queue()
+    queue.enqueue([start])
+    while queue.size() > 0:
+        path = queue.dequeue()
+        lastItemInPath = path[-1]
+        if lastItemInPath not in visited:
+            for exit in graph[lastItemInPath]:
+                if graph[lastItemInPath][exit] == '?':
+                    return path
+            visited.add(lastItemInPath)
+            for new_exit in graph[lastItemInPath]:
+                path_copy = path.copy()
+                path_copy.append(graph[lastItemInPath][new_exit])
+                queue.enqueue(path_copy)
+    return []
+
+def getDirectionsFromPath(path):
+    directions = []
+    current_room = path[0]
+    path.remove(current_room)
+    for room in path:
+        for exit in graph[current_room]:
+            if room == graph[current_room][exit]:
+                directions.append(exit)
+    return directions
+
+
+while True:
+    room_exits = graph[player.currentRoom.id]
+    directionList = []
+
+    for direction in room_exits:
+        if room_exits[direction] == '?':
+            directionList.append(direction)
+
+
+    if len(directionList) > 0:
+        exitDirection = directionList[0]
+        traversalPath.append(exitDirection)
+        oldRoomId = player.currentRoom.id
+        player.travel(exitDirection)
+        unknownRoom = {}
+
+        if player.currentRoom.id not in graph:
+            for exit in player.currentRoom.getExits():
+                unknownRoom[exit] = '?'
+            graph[player.currentRoom.id] = unknownRoom
+        graph[oldRoomId][exitDirection] = player.currentRoom.id
+        graph[player.currentRoom.id][oppositeLookup[exitDirection]] = oldRoomId
+    else:
+        walkDirections = bfs(player.currentRoom.id)
+        if len(walkDirections) == 0:
+            break
+        for direction in getDirectionsFromPath(walkDirections):
+            player.travel(direction)
+            traversalPath.append(direction)
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -36,11 +104,11 @@ for move in traversalPath:
     visited_rooms.add(player.currentRoom)
 
 if len(visited_rooms) == len(roomGraph):
-    print(f"TESTS PASSED: {len(traversalPath)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversalPath)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(roomGraph) - len(visited_rooms)} unvisited rooms")
-
 
 
 #######
